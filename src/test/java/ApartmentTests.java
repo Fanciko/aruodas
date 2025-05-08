542251d2df64fb521b415004e71244ff5b619b2d
Fanciko <jonis161@gmail.com>
2025-05-08 13:50:36 +0300

    Added tests options.

--- src/test/java/ApartmentTests.java
+++ src/test/java/ApartmentTests.java
@@ -1,6 +1,8 @@
 import org.example.Helper;
 import org.example.models.Apartament;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -21,7 +23,158 @@ public class ApartmentTests {
 
     @Test
     public void positiveTest() {
-        Apartament a = new Apartament("Klaipeda", "Klaipėdos m.", "laukinink", "laukinink", "1111-1111-1111", true, false, "Geras butas", "Nice apartment", "Paruski nepagavari", new String[]{"C:\\Users\\Jonas\\IdeaProjects\\aruodas\\src\\photos\\parduodamas.jpg"}, "www.youtube.com/gerasbutas", "www.3dtour.com/gerasbutas", "127999", "64345599", "jonas@moneymaker.com", true, true, 1, true, "27", "22", "64", 3, "9", "9", "1994", 1, 1, new int[]{1, 2}, 1, new int[]{1, 2}, new int[]{2, 4, 13, 15, 22, 26, 35, 37}, 1);
+        Apartament a = new Apartament("Klaipeda", "Klaipėdos m.", "laukinink", "laukinink", "1111-1111-1111", true, false, "Geras butas", "Nice apartment", "Paruski nepagavari", new String[]{"C:\\Users\\Jonas\\IdeaProjects\\aruodas\\src\\photos\\parduodamas.jpg"}, "https://www.youtube.com/watch?v=xawlGMAdpAo", "www.3dtour.com/gerasbutas", "127999", "64345599", "jonas@moneymaker.com", true, true, 1, true, "27", "22", "64", 3, "9", "9", "1994", 1, 1, new int[]{1, 2}, 1, new int[]{1, 2}, new int[]{2, 4, 13, 15, 22, 26, 35, 37}, 1);
         a.fill();
+        driver.findElement(By.xpath("//*[@id=\"newObjectForm\"]/ul/li[22]/div/div[1]/div[2]")).click();
+        driver.findElement(By.id("submitFormButton")).click();
+        wait(3000);
+        String actual = "";
+        try {
+            actual = driver.findElement(By.xpath("/html/body/div[1]/div[1]/div[2]/div/span")).getText();
+        } catch (Exception e) {
+        }
+        Assert.assertEquals(actual, "Paslaugų paketo pasirinkimas");
+    }
+
+    @Test
+    public void withoutRcnumber() {
+        Apartament a = new Apartament("Klaipeda", "Klaipėdos m.", "laukinink", "laukinink", "", true, false, "Geras butas", "Nice apartment", "Paruski nepagavari", new String[]{"C:\\Users\\Jonas\\IdeaProjects\\aruodas\\src\\photos\\parduodamas.jpg"}, "https://www.youtube.com/watch?v=xawlGMAdpAo", "www.3dtour.com/gerasbutas", "127999", "64345599", "jonas@moneymaker.com", true, true, 1, true, "27", "22", "64", 3, "9", "9", "1994", 1, 1, new int[]{1, 2}, 1, new int[]{1, 2}, new int[]{2, 4, 13, 15, 22, 26, 35, 37}, 1);
+        a.fill();
+        driver.findElement(By.xpath("//*[@id=\"newObjectForm\"]/ul/li[22]/div/div[1]/div[2]")).click();
+        driver.findElement(By.id("submitFormButton")).click();
+        wait(3000);
+        String actual = "";
+        try {
+            actual = driver.findElement(By.xpath("/html/body/div[1]/div[1]/div[2]/div/span")).getText();
+        } catch (Exception e) {
+        }
+        Assert.assertEquals(actual, "Paslaugų paketo pasirinkimas");
+    }
+
+    @Test
+    public void withoutNotes() {
+        Apartament a = new Apartament("Klaipeda", "Klaipėdos m.", "laukinink", "laukinink", "1111-1111-1111", true, false, "", "", "", new String[]{"C:\\Users\\Jonas\\IdeaProjects\\aruodas\\src\\photos\\parduodamas.jpg"}, "https://www.youtube.com/watch?v=xawlGMAdpAo", "www.3dtour.com/gerasbutas", "127999", "64345599", "jonas@moneymaker.com", true, true, 1, true, "27", "22", "64", 3, "9", "9", "1994", 1, 1, new int[]{1, 2}, 1, new int[]{1, 2}, new int[]{2, 4, 13, 15, 22, 26, 35, 37}, 1);
+        a.fill();
+        driver.findElement(By.xpath("//*[@id=\"newObjectForm\"]/ul/li[22]/div/div[1]/div[2]")).click();
+        driver.findElement(By.id("submitFormButton")).click();
+        wait(3000);
+        String actual = "";
+        try {
+            actual = driver.findElement(By.xpath("/html/body/div[1]/div[1]/div[2]/div/span")).getText();
+        } catch (Exception e) {
+        }
+        Assert.assertEquals(actual, "Paslaugų paketo pasirinkimas");
+    }
+
+    @Test
+    public void withoutPhoto() {
+        Apartament a = new Apartament("Klaipeda", "Klaipėdos m.", "laukinink", "laukinink", "1111-1111-1111", true, false, "Geras butas", "Nice apartment", "Paruski nepagavari", new String[]{""}, "https://www.youtube.com/watch?v=xawlGMAdpAo", "www.3dtour.com/gerasbutas", "127999", "64345599", "jonas@moneymaker.com", true, true, 1, true, "27", "22", "64", 3, "9", "9", "1994", 1, 1, new int[]{1, 2}, 1, new int[]{1, 2}, new int[]{2, 4, 13, 15, 22, 26, 35, 37}, 1);
+        a.fill();
+        driver.findElement(By.xpath("//*[@id=\"newObjectForm\"]/ul/li[22]/div/div[1]/div[2]")).click();
+        driver.findElement(By.id("submitFormButton")).click();
+        wait(3000);
+        String actual = "";
+        try {
+            actual = driver.findElement(By.xpath("/html/body/div[1]/div[1]/div[2]/div/span")).getText();
+        } catch (Exception e) {
+        }
+        Assert.assertEquals(actual, "Paslaugų paketo pasirinkimas");
+    }
+
+    @Test
+    public void withoutLocation() {
+        Apartament a = new Apartament("", "", "", "", "1111-1111-1111", true, false, "Geras butas", "Nice apartment", "Paruski nepagavari", new String[]{"C:\\Users\\Jonas\\IdeaProjects\\aruodas\\src\\photos\\parduodamas.jpg"}, "https://www.youtube.com/watch?v=xawlGMAdpAo", "www.3dtour.com/gerasbutas", "127999", "64345599", "jonas@moneymaker.com", true, true, 1, true, "27", "22", "64", 3, "9", "9", "1994", 1, 1, new int[]{1, 2}, 1, new int[]{1, 2}, new int[]{2, 4, 13, 15, 22, 26, 35, 37}, 1);
+        a.fill();
+        driver.findElement(By.xpath("//*[@id=\"newObjectForm\"]/ul/li[22]/div/div[1]/div[2]")).click();
+        driver.findElement(By.id("submitFormButton")).click();
+        wait(3000);
+        String actual = "";
+        try {
+            actual = driver.findElement(By.xpath("/html/body/div[1]/div[1]/div[2]/div/span")).getText();
+        } catch (Exception e) {
+        }
+        Assert.assertEquals(actual, "Paslaugų paketo pasirinkimas");
+    }
+
+    @Test
+    public void withoutPhoneNumber() {
+        Apartament a = new Apartament("Klaipeda", "Klaipėdos m.", "laukinink", "laukinink", "1111-1111-1111", true, false, "Geras butas", "Nice apartment", "Paruski nepagavari", new String[]{"C:\\Users\\Jonas\\IdeaProjects\\aruodas\\src\\photos\\parduodamas.jpg"}, "https://www.youtube.com/watch?v=xawlGMAdpAo", "www.3dtour.com/gerasbutas", "127999", "", "jonas@moneymaker.com", true, true, 1, true, "27", "22", "64", 3, "9", "9", "1994", 1, 1, new int[]{1, 2}, 1, new int[]{1, 2}, new int[]{2, 4, 13, 15, 22, 26, 35, 37}, 1);
+        a.fill();
+        driver.findElement(By.xpath("//*[@id=\"newObjectForm\"]/ul/li[22]/div/div[1]/div[2]")).click();
+        driver.findElement(By.id("submitFormButton")).click();
+        wait(3000);
+        String actual = "";
+        try {
+            actual = driver.findElement(By.className("error-arrow")).getText();
+        } catch (Exception e) {
+        }
+        Assert.assertEquals(actual, "Neteisingas telefono numeris");
+    }
+
+    @Test
+    public void withoutHouseNumber() {
+        Apartament a = new Apartament("Klaipeda", "Klaipėdos m.", "laukinink", "laukinink", "1111-1111-1111", true, false, "Geras butas", "Nice apartment", "Paruski nepagavari", new String[]{"C:\\Users\\Jonas\\IdeaProjects\\aruodas\\src\\photos\\parduodamas.jpg"}, "https://www.youtube.com/watch?v=xawlGMAdpAo", "www.3dtour.com/gerasbutas", "127999", "64345599", "jonas@moneymaker.com", true, true, 1, true, "", "22", "64", 3, "9", "9", "1994", 1, 1, new int[]{1, 2}, 1, new int[]{1, 2}, new int[]{2, 4, 13, 15, 22, 26, 35, 37}, 1);
+        a.fill();
+        driver.findElement(By.xpath("//*[@id=\"newObjectForm\"]/ul/li[22]/div/div[1]/div[2]")).click();
+        driver.findElement(By.id("submitFormButton")).click();
+        wait(3000);
+        String actual = "";
+        try {
+            actual = driver.findElement(By.xpath("/html/body/div[1]/div[1]/div[2]/div/span")).getText();
+        } catch (Exception e) {
+        }
+        Assert.assertEquals(actual, "Paslaugų paketo pasirinkimas");
+    }
+
+    @Test
+    public void withoutPlot() {
+        Apartament a = new Apartament("Klaipeda", "Klaipėdos m.", "laukinink", "laukinink", "1111-1111-1111", true, false, "Geras butas", "Nice apartment", "Paruski nepagavari", new String[]{"C:\\Users\\Jonas\\IdeaProjects\\aruodas\\src\\photos\\parduodamas.jpg"}, "https://www.youtube.com/watch?v=xawlGMAdpAo", "www.3dtour.com/gerasbutas", "127999", "64345599", "jonas@moneymaker.com", true, true, 1, true, "27", "22", "", 3, "9", "9", "1994", 1, 1, new int[]{1, 2}, 1, new int[]{1, 2}, new int[]{2, 4, 13, 15, 22, 26, 35, 37}, 1);
+        a.fill();
+        driver.findElement(By.xpath("//*[@id=\"newObjectForm\"]/ul/li[22]/div/div[1]/div[2]")).click();
+        driver.findElement(By.id("submitFormButton")).click();
+        wait(3000);
+        String actual = "";
+        try {
+            actual = driver.findElement(By.className("error-arrow")).getText();
+        } catch (Exception e) {
+        }
+        Assert.assertEquals(actual, "Įveskite bendrą plotą");
+    }
+
+    @Test
+    public void WithoutPrice() {
+        Apartament a = new Apartament("Klaipeda", "Klaipėdos m.", "laukinink", "laukinink", "1111-1111-1111", true, false, "Geras butas", "Nice apartment", "Paruski nepagavari", new String[]{"C:\\Users\\Jonas\\IdeaProjects\\aruodas\\src\\photos\\parduodamas.jpg"}, "https://www.youtube.com/watch?v=xawlGMAdpAo", "www.3dtour.com/gerasbutas", "", "64345599", "jonas@moneymaker.com", true, true, 1, true, "27", "22", "64", 3, "9", "9", "1994", 1, 1, new int[]{1, 2}, 1, new int[]{1, 2}, new int[]{2, 4, 13, 15, 22, 26, 35, 37}, 1);
+        a.fill();
+        driver.findElement(By.xpath("//*[@id=\"newObjectForm\"]/ul/li[22]/div/div[1]/div[2]")).click();
+        driver.findElement(By.id("submitFormButton")).click();
+        wait(3000);
+        String actual = "";
+        try {
+            actual = driver.findElement(By.className("error-arrow")).getText();
+        } catch (Exception e) {
+        }
+        Assert.assertEquals(actual, "Neteisinga kaina");
+    }
+
+    @Test
+    public void withoutEmail() {
+        Apartament a = new Apartament("Klaipeda", "Klaipėdos m.", "laukinink", "laukinink", "1111-1111-1111", true, false, "Geras butas", "Nice apartment", "Paruski nepagavari", new String[]{"C:\\Users\\Jonas\\IdeaProjects\\aruodas\\src\\photos\\parduodamas.jpg"}, "https://www.youtube.com/watch?v=xawlGMAdpAo", "www.3dtour.com/gerasbutas", "127999", "64345599", "", true, true, 1, true, "27", "22", "64", 3, "9", "9", "1994", 1, 1, new int[]{1, 2}, 1, new int[]{1, 2}, new int[]{2, 4, 13, 15, 22, 26, 35, 37}, 1);
+        a.fill();
+        driver.findElement(By.xpath("//*[@id=\"newObjectForm\"]/ul/li[22]/div/div[1]/div[2]")).click();
+        driver.findElement(By.id("submitFormButton")).click();
+        wait(3000);
+        String actual = "";
+        try {
+            actual = driver.findElement(By.className("error-arrow")).getText();
+        } catch (Exception e) {
+        }
+        Assert.assertEquals(actual, "Nurodykite el. pašto adresą, kad vėliau galėtumėte redaguoti arba pašalinti skelbimą");
+    }
+
+    public void wait(int time) {
+        try {
+            Thread.sleep(time);
+        } catch (Exception e) {
+        }
     }
 }
--- src/test/java/PlotTests.java
+++ src/test/java/PlotTests.java
@@ -1,6 +1,8 @@
 import org.example.Helper;
 import org.example.models.Plot;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -21,7 +23,146 @@ public class PlotTests {
 
     @Test
     public void positiveTest() {
-        Plot a = new Plot("Klaipeda", "Klaipėdos m.", "laukinink", "laukinink", "1111-1111-1111", true, false, "Parduodu sklypa", "selling a plot", "Ruski karabliki***", new String[]{"C:\\Users\\Jonas\\IdeaProjects\\aruodas\\src\\photos\\parduodamas.jpg"}, "https://www.youtube.com/watch?v=xawlGMAdpAo", "https://www.klapty.com/p/mag-b/t/example-virtual-tour-apartment-1-41145", "279999", "64345599", "jonas@moneymaker.com", true, true, 4, true, "27", "22", new int[]{1, 2, 3}, new int[]{1, 2, 3});
+        Plot a = new Plot("Klaipeda", "Klaipėdos m.", "laukinink", "laukinink", "1111-1111-1111", true, false, "Parduodu sklypa", "selling a plot", "Ruski nypanimaju", new String[]{"C:\\Users\\Jonas\\IdeaProjects\\aruodas\\src\\photos\\parduodamas.jpg"}, "https://www.youtube.com/watch?v=xawlGMAdpAo", "https://www.klapty.com/p/mag-b/t/example-virtual-tour-apartment-1-41145", "279999", "64345599", "jonas@moneymaker.com", true, true, 4, true, "27", "22", new int[]{1, 2, 3}, new int[]{1, 2, 3});
         a.fill();
+        driver.findElement(By.id("submitFormButton")).click();
+        wait(3000);
+        String actual = "";
+        try {
+            actual = driver.findElement(By.xpath("/html/body/div[1]/div[1]/div[2]/div/span")).getText();
+        } catch (Exception e) {
+        }
+        Assert.assertEquals(actual, "Paslaugų paketo pasirinkimas");
+    }
+
+    @Test
+    public void withoutRcnumber() {
+        Plot a = new Plot("Klaipeda", "Klaipėdos m.", "laukinink", "laukinink", "", true, false, "Parduodu sklypa", "selling a plot", "Ruski nypanimaju", new String[]{"C:\\Users\\Jonas\\IdeaProjects\\aruodas\\src\\photos\\parduodamas.jpg"}, "https://www.youtube.com/watch?v=xawlGMAdpAo", "https://www.klapty.com/p/mag-b/t/example-virtual-tour-apartment-1-41145", "279999", "64345599", "jonas@moneymaker.com", true, true, 4, true, "27", "22", new int[]{1, 2, 3}, new int[]{1, 2, 3});
+        a.fill();
+        String actual = "";
+        try {
+            actual = driver.findElement(By.xpath("/html/body/div[1]/div[1]/div[2]/div/span")).getText();
+        } catch (Exception e) {
+        }
+        Assert.assertEquals(actual, "Paslaugų paketo pasirinkimas");
+    }
+
+    @Test
+    public void withoutNotes() {
+        Plot a = new Plot("Klaipeda", "Klaipėdos m.", "laukinink", "laukinink", "1111-1111-1111", true, false, "", "", "", new String[]{"C:\\Users\\Jonas\\IdeaProjects\\aruodas\\src\\photos\\parduodamas.jpg"}, "https://www.youtube.com/watch?v=xawlGMAdpAo", "https://www.klapty.com/p/mag-b/t/example-virtual-tour-apartment-1-41145", "279999", "64345599", "jonas@moneymaker.com", true, true, 4, true, "27", "22", new int[]{1, 2, 3}, new int[]{1, 2, 3});
+        a.fill();
+        String actual = "";
+        try {
+            actual = driver.findElement(By.xpath("/html/body/div[1]/div[1]/div[2]/div/span")).getText();
+        } catch (Exception e) {
+        }
+        Assert.assertEquals(actual, "Paslaugų paketo pasirinkimas");
+    }
+
+    @Test
+    public void withoutPhoto() {
+        Plot a = new Plot("Klaipeda", "Klaipėdos m.", "laukinink", "laukinink", "1111-1111-1111", true, false, "Parduodu sklypa", "selling a plot", "Ruski nypanimaju", new String[]{""}, "https://www.youtube.com/watch?v=xawlGMAdpAo", "https://www.klapty.com/p/mag-b/t/example-virtual-tour-apartment-1-41145", "279999", "64345599", "jonas@moneymaker.com", true, true, 4, true, "27", "22", new int[]{1, 2, 3}, new int[]{1, 2, 3});
+        a.fill();
+        driver.findElement(By.id("submitFormButton")).click();
+        wait(3000);
+        String actual = "";
+        try {
+            actual = driver.findElement(By.xpath("/html/body/div[1]/div[1]/div[2]/div/span")).getText();
+        } catch (Exception e) {
+        }
+        Assert.assertEquals(actual, "Paslaugų paketo pasirinkimas");
+    }
+
+    @Test
+    public void withoutLocation() {
+        Plot a = new Plot("", "", "", "", "1111-1111-1111", true, false, "Parduodu sklypa", "selling a plot", "Ruski nypanimaju", new String[]{"C:\\Users\\Jonas\\IdeaProjects\\aruodas\\src\\photos\\parduodamas.jpg"}, "https://www.youtube.com/watch?v=xawlGMAdpAo", "https://www.klapty.com/p/mag-b/t/example-virtual-tour-apartment-1-41145", "279999", "64345599", "jonas@moneymaker.com", true, true, 4, true, "27", "22", new int[]{1, 2, 3}, new int[]{1, 2, 3});
+        a.fill();
+        driver.findElement(By.id("submitFormButton")).click();
+        wait(3000);
+        String actual = "";
+        try {
+            actual = driver.findElement(By.xpath("/html/body/div[1]/div[1]/div[2]/div/span")).getText();
+        } catch (Exception e) {
+        }
+        Assert.assertEquals(actual, "Paslaugų paketo pasirinkimas");
+    }
+
+    @Test
+    public void withoutPhoneNumber() {
+        Plot a = new Plot("Klaipeda", "Klaipėdos m.", "laukinink", "laukinink", "1111-1111-1111", true, false, "Parduodu sklypa", "selling a plot", "Ruski nypanimaju", new String[]{"C:\\Users\\Jonas\\IdeaProjects\\aruodas\\src\\photos\\parduodamas.jpg"}, "https://www.youtube.com/watch?v=xawlGMAdpAo", "https://www.klapty.com/p/mag-b/t/example-virtual-tour-apartment-1-41145", "279999", "", "jonas@moneymaker.com", true, true, 4, true, "27", "22", new int[]{1, 2, 3}, new int[]{1, 2, 3});
+        a.fill();
+        driver.findElement(By.id("submitFormButton")).click();
+        wait(3000);
+        String actual = "";
+        try {
+            actual = driver.findElement(By.className("error-arrow")).getText();
+        } catch (Exception e) {
+        }
+        Assert.assertEquals(actual, "Neteisingas telefono numeris");
+    }
+
+    @Test
+    public void withoutHouseNumber() {
+        Plot a = new Plot("Klaipeda", "Klaipėdos m.", "laukinink", "laukinink", "1111-1111-1111", true, false, "Parduodu sklypa", "selling a plot", "Ruski nypanimaju", new String[]{"C:\\Users\\Jonas\\IdeaProjects\\aruodas\\src\\photos\\parduodamas.jpg"}, "https://www.youtube.com/watch?v=xawlGMAdpAo", "https://www.klapty.com/p/mag-b/t/example-virtual-tour-apartment-1-41145", "279999", "64345599", "jonas@moneymaker.com", true, true, 4, true, "", "22", new int[]{1, 2, 3}, new int[]{1, 2, 3});
+        a.fill();
+        driver.findElement(By.id("submitFormButton")).click();
+        wait(3000);
+        String actual = "";
+        try {
+            actual = driver.findElement(By.xpath("/html/body/div[1]/div[1]/div[2]/div/span")).getText();
+        } catch (Exception e) {
+        }
+        Assert.assertEquals(actual, "Paslaugų paketo pasirinkimas");
+    }
+
+    @Test
+    public void withoutHectar() {
+        Plot a = new Plot("Klaipeda", "Klaipėdos m.", "laukinink", "laukinink", "1111-1111-1111", true, false, "Parduodu sklypa", "selling a plot", "Ruski nypanimaju", new String[]{"C:\\Users\\Jonas\\IdeaProjects\\aruodas\\src\\photos\\parduodamas.jpg"}, "https://www.youtube.com/watch?v=xawlGMAdpAo", "https://www.klapty.com/p/mag-b/t/example-virtual-tour-apartment-1-41145", "279999", "64345599", "jonas@moneymaker.com", true, true, 4, true, "27", "", new int[]{1, 2, 3}, new int[]{1, 2, 3});
+        a.fill();
+        driver.findElement(By.id("submitFormButton")).click();
+        wait(3000);
+        String actual = "";
+        try {
+            actual = driver.findElement(By.className("error-arrow")).getText();
+        } catch (Exception e) {
+        }
+        Assert.assertEquals(actual, "Įveskite bendrą plotą");
+    }
+
+    @Test
+    public void withoutPrice() {
+        Plot a = new Plot("Klaipeda", "Klaipėdos m.", "laukinink", "laukinink", "1111-1111-1111", true, false, "Parduodu sklypa", "selling a plot", "Ruski nypanimaju", new String[]{"C:\\Users\\Jonas\\IdeaProjects\\aruodas\\src\\photos\\parduodamas.jpg"}, "https://www.youtube.com/watch?v=xawlGMAdpAo", "https://www.klapty.com/p/mag-b/t/example-virtual-tour-apartment-1-41145", "", "64345599", "jonas@moneymaker.com", true, true, 4, true, "27", "22", new int[]{1, 2, 3}, new int[]{1, 2, 3});
+        a.fill();
+        driver.findElement(By.id("submitFormButton")).click();
+        wait(3000);
+        String actual = "";
+        try {
+            actual = driver.findElement(By.className("error-arrow")).getText();
+        } catch (Exception e) {
+        }
+        Assert.assertEquals(actual, "Neteisinga kaina");
+    }
+
+    @Test
+    public void withoutEmail() {
+        Plot a = new Plot("Klaipeda", "Klaipėdos m.", "laukinink", "laukinink", "1111-1111-1111", true, false, "Parduodu sklypa", "selling a plot", "Ruski nypanimaju", new String[]{"C:\\Users\\Jonas\\IdeaProjects\\aruodas\\src\\photos\\parduodamas.jpg"}, "https://www.youtube.com/watch?v=xawlGMAdpAo", "https://www.klapty.com/p/mag-b/t/example-virtual-tour-apartment-1-41145", "279999", "64345599", "", true, true, 4, true, "27", "22", new int[]{1, 2, 3}, new int[]{1, 2, 3});
+        a.fill();
+        driver.findElement(By.id("submitFormButton")).click();
+        wait(3000);
+        String actual = "";
+        try {
+            actual = driver.findElement(By.className("error-arrow")).getText();
+        } catch (Exception e) {
+        }
+        Assert.assertEquals(actual, "Nurodykite el. pašto adresą, kad vėliau galėtumėte redaguoti arba pašalinti skelbimą");
+    }
+
+    public void wait(int time) {
+        try {
+            Thread.sleep(time);
+        } catch (Exception e) {
+        }
     }
 }
+
+
